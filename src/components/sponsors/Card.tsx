a2026cb2c6be7f9cb09230b8a60efa529916c5e0
Adith1207 <adithnarayang@gmail.com>
2026-01-29 19:57:02 +0530

    handled the large empty spacing in mob view

--- src/components/sponsors/Card.tsx
+++ src/components/sponsors/Card.tsx
@@ -1,28 +1,28 @@
 import { motion } from "framer-motion";
 
 const Card = (props: {
-  name: string;
-  img: string;
-  extraStyling?: string;
-  appearDelay?: number;
-  animationDuration?: number;
+	name: string;
+	img: string;
+	extraStyling?: string;
+	appearDelay?: number;
+	animationDuration?: number;
 }) => {
-  const { name, img, extraStyling, appearDelay, animationDuration } = props;
+	const { name, img, extraStyling, appearDelay, animationDuration } = props;
 
-  return (
-    <motion.div
-      initial={{ translateY: 120, opacity: 0 }}
-      animate={{ translateY: 0, opacity: 1 }}
-      transition={{
-        duration: animationDuration ?? 1,
-        ease: "easeInOut",
-        delay: appearDelay ?? 1,
-      }}
-      className="flex flex-col items-center gap-6 w-full max-w-xl"
-    >
-      {/* Card Title */}
-      <p
-        className="
+	return (
+		<motion.div
+			initial={{ translateY: 120, opacity: 0 }}
+			animate={{ translateY: 0, opacity: 1 }}
+			transition={{
+				duration: animationDuration ?? 1,
+				ease: "easeInOut",
+				delay: appearDelay ?? 1,
+			}}
+			className="flex flex-col items-center gap-6 w-full max-w-xl"
+		>
+			{/* Card Title */}
+			<p
+				className="
           text-[#f4d03e]
           font-jersey
           text-4xl md:text-5xl
@@ -30,32 +30,32 @@ const Card = (props: {
           text-center
           whitespace-nowrap
         "
-      >
-        {name}
-      </p>
+			>
+				{name}
+			</p>
 
-      {/* Card Body */}
-      <div
-        className="
+			{/* Card Body */}
+			<div
+				className="
           w-full
           flex justify-center items-center
           bg-[url(/sponsors-page/card.png)]
           bg-center bg-contain bg-no-repeat
           py-[12%]
         "
-      >
-        <img
-          src={img}
-          alt={name}
-          className={`
+			>
+				<img
+					src={img}
+					alt={name}
+					className={`
             w-[70%] md:w-[60%]
             object-contain
             ${extraStyling ?? ""}
           `}
-        />
-      </div>
-    </motion.div>
-  );
+				/>
+			</div>
+		</motion.div>
+	);
 };
 
 export default Card;
